47251c1164307599d00f4ed64e1a33caa8ea51d9
boggedbrush <90526147+boggedbrush@users.noreply.github.com>
2026-02-06 20:14:12 -0500

    refactor: Replace hardcoded UI colors with UIManager colors for improved theming and readability.

--- source/net/filebot/ui/rename/FormatDialog.java
+++ source/net/filebot/ui/rename/FormatDialog.java
@@ -107,7 +107,8 @@ public class FormatDialog extends JDialog {
 	private JLabel title = new JLabel();
 	private JPanel help = new JPanel(new MigLayout("insets 0, nogrid, novisualpadding, fillx"));
 
-	private static final PreferencesEntry<String> persistentSampleFile = Settings.forPackage(FormatDialog.class).entry("format.sample.file");
+	private static final PreferencesEntry<String> persistentSampleFile = Settings.forPackage(FormatDialog.class)
+			.entry("format.sample.file");
 
 	public enum Mode {
 
@@ -124,14 +125,14 @@ public class FormatDialog extends JDialog {
 
 		public Format getFormat() {
 			switch (this) {
-			case Episode:
-				return new EpisodeFormat();
-			case Movie:
-				return new MovieFormat(true, true, false);
-			case Music:
-				return new AudioTrackFormat();
-			default:
-				return new FileNameFormat();
+				case Episode:
+					return new EpisodeFormat();
+				case Movie:
+					return new MovieFormat(true, true, false);
+				case Music:
+					return new AudioTrackFormat();
+				default:
+					return new FileNameFormat();
 			}
 		}
 
@@ -198,8 +199,9 @@ public class FormatDialog extends JDialog {
 
 		JPanel header = new JPanel(new MigLayout("insets dialog, nogrid, novisualpadding"));
 
-		header.setBackground(Color.white);
-		header.setBorder(new SeparatorBorder(1, new Color(0xB4B4B4), new Color(0xACACAC), GradientStyle.LEFT_TO_RIGHT, Position.BOTTOM));
+		header.setBackground(javax.swing.UIManager.getColor("Panel.background"));
+		header.setBorder(new SeparatorBorder(1, javax.swing.UIManager.getColor("Component.borderColor"),
+				javax.swing.UIManager.getColor("Component.borderColor"), GradientStyle.LEFT_TO_RIGHT, Position.BOTTOM));
 
 		header.add(progressIndicator, "pos 1al 0al, hidemode 3");
 		header.add(title, "wmin 150px, wrap unrel:push");
@@ -222,8 +224,10 @@ public class FormatDialog extends JDialog {
 
 		content.add(editorScrollPane, "w 120px:min(pref, 420px), h pref, growx, wrap 4px, id editor");
 		content.add(createImageButton(changeSampleAction), "sg action, w 25!, h 19!, pos n editor.y2+2 editor.x2 n");
-		content.add(createImageButton(selectFolderAction), "sg action, w 25!, h 19!, pos n editor.y2+2 editor.x2-(27*1) n");
-		content.add(createImageButton(showRecentAction), "sg action, w 25!, h 19!, pos n editor.y2+2 editor.x2-(27*2) n");
+		content.add(createImageButton(selectFolderAction),
+				"sg action, w 25!, h 19!, pos n editor.y2+2 editor.x2-(27*1) n");
+		content.add(createImageButton(showRecentAction),
+				"sg action, w 25!, h 19!, pos n editor.y2+2 editor.x2-(27*2) n");
 
 		content.add(help, "growx, wrap 25px:push");
 
@@ -302,7 +306,8 @@ public class FormatDialog extends JDialog {
 		}
 
 		// install show / hide help keyboard shortcut
-		installAction(this.getRootPane(), getKeyStroke(VK_F1, 0), newAction("Help", evt -> help.setVisible(!help.isVisible())));
+		installAction(this.getRootPane(), getKeyStroke(VK_F1, 0),
+				newAction("Help", evt -> help.setVisible(!help.isVisible())));
 
 		// initialize data
 		setState(initMode, bindings, locked);
@@ -314,7 +319,8 @@ public class FormatDialog extends JDialog {
 
 		if (locked) {
 			this.setTitle(String.format("%s Format", mode));
-			title.setText(String.format("%s ⇔ %s", mode, bindings.getInfoObject(), bindings.getFileObject() == null ? null : bindings.getFileObject().getName()));
+			title.setText(String.format("%s ⇔ %s", mode, bindings.getInfoObject(),
+					bindings.getFileObject() == null ? null : bindings.getFileObject().getName()));
 		} else {
 			this.setTitle(String.format("%s Format", mode));
 			title.setText(String.format("%s Format", mode));
@@ -330,7 +336,8 @@ public class FormatDialog extends JDialog {
 		sample = bindings;
 
 		// restore editor state
-		setFormatCode(mode.persistentFormatHistory().isEmpty() ? mode.getDefaultFormatExpression() : mode.persistentFormatHistory().get(0));
+		setFormatCode(mode.persistentFormatHistory().isEmpty() ? mode.getDefaultFormatExpression()
+				: mode.persistentFormatHistory().get(0));
 
 		// update examples
 		fireSampleChanged();
@@ -368,13 +375,14 @@ public class FormatDialog extends JDialog {
 
 	private JComponent createSyntaxPanel(Mode mode) {
 		JPanel panel = new JPanel(new MigLayout("fill, nogrid, novisualpadding", "[pref]", "[fill, min]"));
-		panel.setBorder(createLineBorder(new Color(0xACA899)));
-		panel.setBackground(new Color(0xFFFFE1));
+		panel.setBorder(createLineBorder(javax.swing.UIManager.getColor("Component.borderColor")));
+		panel.setBackground(javax.swing.UIManager.getColor("TextArea.background"));
 		panel.setOpaque(true);
 
-		panel.add(new LinkButton(newAction(ResourceBundle.getBundle(FormatDialog.class.getName()).getString(mode.key() + ".syntax"), evt -> {
-			openURI(ResourceBundle.getBundle(FormatDialog.class.getName()).getString("help.url"));
-		})), "h min!");
+		panel.add(new LinkButton(newAction(
+				ResourceBundle.getBundle(FormatDialog.class.getName()).getString(mode.key() + ".syntax"), evt -> {
+					openURI(ResourceBundle.getBundle(FormatDialog.class.getName()).getString("help.url"));
+				})), "h min!");
 
 		return panel;
 	}
@@ -382,8 +390,8 @@ public class FormatDialog extends JDialog {
 	private JComponent createExamplesPanel(Mode mode) {
 		JPanel panel = new JPanel(new MigLayout("fill, wrap 3"));
 
-		panel.setBorder(createLineBorder(new Color(0xACA899)));
-		panel.setBackground(new Color(0xFFFFE1));
+		panel.setBorder(createLineBorder(javax.swing.UIManager.getColor("Component.borderColor")));
+		panel.setBackground(javax.swing.UIManager.getColor("TextArea.background"));
 
 		for (String format : mode.getSampleExpressions()) {
 			LinkButton formatLink = new LinkButton(newAction(format, e -> setFormatCode(format)));
@@ -439,7 +447,8 @@ public class FormatDialog extends JDialog {
 
 	private ExecutorService createExecutor() {
 		// only keep the latest task in the queue
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(1), new DefaultThreadFactory("PreviewFormatter"));
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.SECONDS,
+				new LinkedBlockingQueue<Runnable>(1), new DefaultThreadFactory("PreviewFormatter"));
 		executor.setRejectedExecutionHandler(new DiscardOldestPolicy());
 		return executor;
 	}
@@ -449,7 +458,8 @@ public class FormatDialog extends JDialog {
 			// check syntax in foreground
 			ExpressionFileFormat format = new ExpressionFileFormat(editor.getText().trim());
 
-			// activate delayed to avoid flickering when formatting takes only a couple of milliseconds
+			// activate delayed to avoid flickering when formatting takes only a couple of
+			// milliseconds
 			Timer progressIndicatorTimer = invokeLater(400, () -> progressIndicator.setVisible(true));
 
 			// cancel old worker later
@@ -484,7 +494,8 @@ public class FormatDialog extends JDialog {
 						if (suppressed != null) {
 							BindingException bindingIssue = findCause(suppressed, BindingException.class);
 
-							if (bindingIssue != null && getMessage(bindingIssue).contains(MediaBindingBean.EXCEPTION_SAMPLE_FILE_NOT_SET)) {
+							if (bindingIssue != null && getMessage(bindingIssue)
+									.contains(MediaBindingBean.EXCEPTION_SAMPLE_FILE_NOT_SET)) {
 								// exception caused by file bindings because sample file has not been set
 								status.setText(getMessage(bindingIssue));
 								status.setIcon(ResourceManager.getIcon("action.variables"));
@@ -492,7 +503,8 @@ public class FormatDialog extends JDialog {
 								// default binding exception handler
 								status.setText(getMessage(bindingIssue));
 								status.setIcon(ResourceManager.getIcon("status.info"));
-							} else if (suppressed.getCause() != null && suppressed.getCause().getClass() == Exception.class) {
+							} else if (suppressed.getCause() != null
+									&& suppressed.getCause().getClass() == Exception.class) {
 								// ScriptShellMethods throws Exception type exceptions which are not unexpected
 								status.setText(execution.getCause().getMessage());
 								status.setIcon(ResourceManager.getIcon("status.info"));
@@ -523,7 +535,8 @@ public class FormatDialog extends JDialog {
 				}
 			};
 
-			// cancel old worker, after new worker has been created, because done() might be called from within cancel()
+			// cancel old worker, after new worker has been created, because done() might be
+			// called from within cancel()
 			if (obsoletePreviewFuture != null) {
 				obsoletePreviewFuture.cancel(true);
 			}
@@ -593,103 +606,114 @@ public class FormatDialog extends JDialog {
 		return popup;
 	}
 
-	protected final Action changeSampleAction = newAction("Change Sample", ResourceManager.getIcon("action.variables"), evt -> {
-		BindingDialog dialog = new BindingDialog(getWindow(evt.getSource()), String.format("%s Bindings", mode), mode.getFormat(), !locked);
-		dialog.setSample(sample);
+	protected final Action changeSampleAction = newAction("Change Sample", ResourceManager.getIcon("action.variables"),
+			evt -> {
+				BindingDialog dialog = new BindingDialog(getWindow(evt.getSource()), String.format("%s Bindings", mode),
+						mode.getFormat(), !locked);
+				dialog.setSample(sample);
 
-		// open dialog
-		dialog.setLocationRelativeTo((Component) evt.getSource());
-		dialog.setVisible(true);
+				// open dialog
+				dialog.setLocationRelativeTo((Component) evt.getSource());
+				dialog.setVisible(true);
 
-		if (dialog.submit()) {
-			Object info = dialog.getInfoObject();
-			File file = dialog.getMediaFile();
+				if (dialog.submit()) {
+					Object info = dialog.getInfoObject();
+					File file = dialog.getMediaFile();
 
-			// change sample
-			sample = new MediaBindingBean(info, file);
+					// change sample
+					sample = new MediaBindingBean(info, file);
 
-			// remember sample
-			try {
-				mode.persistentSample().setValue(info == null ? "" : MetaAttributes.toJson(info));
-				persistentSampleFile.setValue(file == null ? "" : sample.getFileObject().getAbsolutePath());
-			} catch (Exception e) {
-				debug.log(Level.WARNING, e.getMessage(), e);
-			}
-
-			// reevaluate everything
-			fireSampleChanged();
-		}
-	});
-
-	protected final Action selectFolderAction = newAction("Change Folder", ResourceManager.getIcon("action.load"), evt -> {
-		String relativeFormat = editor.getText().trim();
-		File absoluteFolder = null;
-
-		if (relativeFormat.length() > 0) {
-			File templatePath = new File(relativeFormat);
-			if (templatePath.isAbsolute()) {
-				File existingPath = null;
-				for (File next : listPath(templatePath)) {
-					if (existingPath != null && !next.exists()) {
-						absoluteFolder = existingPath;
-						relativeFormat = relativeFormat.substring(existingPath.getPath().length() + 1); // account for file separator
-						break;
+					// remember sample
+					try {
+						mode.persistentSample().setValue(info == null ? "" : MetaAttributes.toJson(info));
+						persistentSampleFile.setValue(file == null ? "" : sample.getFileObject().getAbsolutePath());
+					} catch (Exception e) {
+						debug.log(Level.WARNING, e.getMessage(), e);
 					}
-					existingPath = next;
+
+					// reevaluate everything
+					fireSampleChanged();
 				}
-			}
-		}
+			});
 
-		File selectedFolder = UserFiles.showOpenDialogSelectFolder(absoluteFolder, "Select Folder", evt);
-		if (selectedFolder != null) {
-			editor.setText(normalizePathSeparators(selectedFolder.getAbsolutePath()) + "/" + relativeFormat);
-		}
-	});
+	protected final Action selectFolderAction = newAction("Change Folder", ResourceManager.getIcon("action.load"),
+			evt -> {
+				String relativeFormat = editor.getText().trim();
+				File absoluteFolder = null;
 
-	protected final Action showRecentAction = newAction("Change Format", ResourceManager.getIcon("action.menu"), evt -> {
-		// display popup below format editor
-		JComponent c = (JComponent) evt.getSource();
-		editor.getComponentPopupMenu().show(c, 0, c.getHeight() + 3);
-	});
+				if (relativeFormat.length() > 0) {
+					File templatePath = new File(relativeFormat);
+					if (templatePath.isAbsolute()) {
+						File existingPath = null;
+						for (File next : listPath(templatePath)) {
+							if (existingPath != null && !next.exists()) {
+								absoluteFolder = existingPath;
+								relativeFormat = relativeFormat.substring(existingPath.getPath().length() + 1); // account
+																												// for
+																												// file
+																												// separator
+								break;
+							}
+							existingPath = next;
+						}
+					}
+				}
+
+				File selectedFolder = UserFiles.showOpenDialogSelectFolder(absoluteFolder, "Select Folder", evt);
+				if (selectedFolder != null) {
+					editor.setText(normalizePathSeparators(selectedFolder.getAbsolutePath()) + "/" + relativeFormat);
+				}
+			});
+
+	protected final Action showRecentAction = newAction("Change Format", ResourceManager.getIcon("action.menu"),
+			evt -> {
+				// display popup below format editor
+				JComponent c = (JComponent) evt.getSource();
+				editor.getComponentPopupMenu().show(c, 0, c.getHeight() + 3);
+			});
 
 	protected final Action cancelAction = newAction("Cancel", ResourceManager.getIcon("dialog.cancel"), evt -> {
 		finish(false);
 	});
 
-	protected final Action switchEditModeAction = newAction("Switch Mode", ResourceManager.getIcon("dialog.switch"), evt -> {
-		Mode next = mode.next();
-		setState(next, restoreSample(next), false);
-	});
+	protected final Action switchEditModeAction = newAction("Switch Mode", ResourceManager.getIcon("dialog.switch"),
+			evt -> {
+				Mode next = mode.next();
+				setState(next, restoreSample(next), false);
+			});
 
-	protected final Action approveFormatAction = newAction("Use Format", ResourceManager.getIcon("dialog.continue"), evt -> {
-		try {
-			// check syntax
-			format = new ExpressionFileFormat(editor.getText().trim());
+	protected final Action approveFormatAction = newAction("Use Format", ResourceManager.getIcon("dialog.continue"),
+			evt -> {
+				try {
+					// check syntax
+					format = new ExpressionFileFormat(editor.getText().trim());
 
-			if (format.getExpression().isEmpty()) {
-				throw new ScriptException("Expression is empty");
-			}
-			if (format.getExpression().length() > Preferences.MAX_VALUE_LENGTH) {
-				throw new ScriptException("Expression is limited to " + Preferences.MAX_VALUE_LENGTH + " characters");
-			}
+					if (format.getExpression().isEmpty()) {
+						throw new ScriptException("Expression is empty");
+					}
+					if (format.getExpression().length() > Preferences.MAX_VALUE_LENGTH) {
+						throw new ScriptException(
+								"Expression is limited to " + Preferences.MAX_VALUE_LENGTH + " characters");
+					}
 
-			// create new recent history and ignore duplicates
-			Set<String> recent = new LinkedHashSet<String>();
+					// create new recent history and ignore duplicates
+					Set<String> recent = new LinkedHashSet<String>();
 
-			// remember the 8 most recent formats
-			recent.add(format.getExpression());
-			recent.addAll(mode.persistentFormatHistory());
+					// remember the 8 most recent formats
+					recent.add(format.getExpression());
+					recent.addAll(mode.persistentFormatHistory());
 
-			// update persistent history
-			mode.persistentFormatHistory().set(recent.stream().filter(Objects::nonNull).limit(8).collect(toList()));
+					// update persistent history
+					mode.persistentFormatHistory()
+							.set(recent.stream().filter(Objects::nonNull).limit(8).collect(toList()));
 
-			finish(true);
-		} catch (ScriptException e) {
-			log.log(Level.WARNING, getRootCauseMessage(e));
-		} catch (Exception e) {
-			log.log(Level.WARNING, e.getMessage(), e);
-		}
-	});
+					finish(true);
+				} catch (ScriptException e) {
+					log.log(Level.WARNING, getRootCauseMessage(e));
+				} catch (Exception e) {
+					log.log(Level.WARNING, e.getMessage(), e);
+				}
+			});
 
 	protected void fireSampleChanged() {
 		firePropertyChange("sample", null, sample);
